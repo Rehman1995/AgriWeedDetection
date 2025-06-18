15d40830c13ad6dc17b63f282a943fc2aba0eec2
Mobeen Ur Rehman <46449452+Rehman1995@users.noreply.github.com>
2025-06-18 22:05:53 +0400

    Update val.py

--- val.py
+++ val.py
@@ -9,7 +9,7 @@ import numpy as np
 import torch
 from tqdm import tqdm
 
-FILE = Path(__file__).resolve()
+FILE = Path(__file__).resolve() 
 ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
